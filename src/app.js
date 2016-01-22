e9f34d8e289a95555e065c3cb03701cb1858d4e0
over-hrk <over.hrk@gmail.com>
2016-01-23 00:57:00 +0900

    stream間の同期の仕組み(event)を実装

--- src/app.js
+++ src/app.js
@@ -18,9 +18,13 @@ var AsyncStream = function(){
             var next = this.actionlist.shift();
             switch(next.label){
                 case "runAction":
+                    cc.log("Exec action=" ,next.action.__instanceId, "(sream=", this.streamID,")");
                     next.target.runAction(next.action);
                     break;
                 case "eventRecord":
+                    next.action.execute();
+                    break;
+                case "eventWait":
                     next.action.execute();
                     break;
                 default:
@@ -39,7 +43,7 @@ var AsyncStream = function(){
         this.actionlist.push({ label : "runAction", target : target, action : wrapperAction });
         this.actionCnt++;
         cc.log("Add action to streamID =", this.streamID, ", size =", this.actionCnt);
-        
+        cc.log("Schedule action=", wrapperAction.__instanceId, "(sream=", this.streamID,")");
         if(this.idle){ 
             // todo:
             this.actionCnt++;
@@ -52,8 +56,11 @@ var AsyncStream = function(){
         
         var wrapperAction = new cc.CallFunc(function(){
             event._recordAction.execute();
+            if( event.callbacks.length > 0 ){
+                event.runCallbacks();
+            }
             check.execute();
-        });
+        }, this);
         
         this.actionlist.push({ label : "eventRecord", action : wrapperAction });
         this.actionCnt++;
@@ -66,6 +73,25 @@ var AsyncStream = function(){
             check.execute();
         }
     };
+    
+    this.eventWaitWrapper = function(event){
+        
+        var wrapperAction = new cc.CallFunc(function(){
+            if( StreamEvent.prototype.isRecorded(event.eventID) ){
+                check.execute();
+            }else{
+                event.addCallback(function(){
+                    check.execute();
+                });
+            }
+        });
+        
+        this.actionlist.push({ label : "eventWait", action : wrapperAction });
+        this.actionCnt++;
+        
+        if(this.idle){ 
+            // todo:
+            this.actionCnt++;
 };
 
 AsyncStream.prototype = {
@@ -76,6 +102,7 @@ var StreamEvent = function(){
     var proto = StreamEvent.prototype;
     this.eventID = proto.eventID++;
     proto.recordedList[this.eventID] = false; 
+    this.callbacks = [];
     
     this._recordAction = new cc.CallFunc(function(){
         var proto = StreamEvent.prototype;
@@ -87,6 +114,20 @@ var StreamEvent = function(){
     
     this.record = function(stream){
         stream.eventRecordWrapper(this);
+    };
+    
+    this.addCallback = function(callback){
+        this.callbacks.push(callback);
+    };
+    
+    this.runCallbacks = function(){
+        for(var key in this.callbacks ){
+            this.callbacks[key]();
+        }
+    };
+    
+    this.wait = function(stream){
+        stream.eventWaitWrapper(this);
     };
 };
 
@@ -153,20 +194,34 @@ var HelloWorldLayer = cc.LayerColor.extend({
         // stream
         var strm1 = new AsyncStream();
         var strm2 = new AsyncStream();
+        var strm3 = new AsyncStream();
         var event1 = new StreamEvent();
+        var event2 = new StreamEvent();
         
         var mov = new cc.MoveTo(1, cc.p(200,30));
         koma_r.runActionAsync(mov, strm1);
         
         var mov2 = new cc.MoveTo(1, cc.p(100,150));
         koma_b.runActionAsync(mov2, strm1);
-        event1.record(strm1);
         
         var mov3 = new cc.MoveTo(1, cc.p(300,150));
         koma_r.runActionAsync(mov3, strm1);
+        event1.record(strm1); // mov3の完了後にevent1がRecordされる
         
         var mov4 = new cc.MoveTo(0.5, cc.p(400,200));
-        koma_b.runActionAsync(mov3,strm2);
+        koma_b.runActionAsync(mov4,strm2);
+        event2.record(strm2); // mov2の完了後にevent2がRecordされる
+        
+        var jumps = [];
+        jumps[0] = cc.JumpBy.create(0.30, cc.p(30,-50), 50,1);
+        jumps[1] = cc.JumpBy.create(0.25, cc.p(30,  0), 40,1);
+        jumps[1] = cc.JumpBy.create(0.20, cc.p(20,  0), 30,1);
+        jumps[2] = cc.JumpBy.create(0.10, cc.p(10,  0),  5,1);
+        jumps[3] = cc.JumpBy.create(0.10, cc.p( 5,  0),  2,1);
+        var jumpMotion = cc.Sequence.create(jumps);
+        event1.wait(strm3); // event1がRecordされるのを待つ
+        event2.wait(strm3); // event2がRecordされるのを待つ
+        koma_b.runActionAsync(jumpMotion,strm3);
         
         return true;
     }
